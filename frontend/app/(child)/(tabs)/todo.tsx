a76efb48769d580c6aee0de2702f2e0f053c1d07
Natali Soto Ferreira <natalixsoto@gmail.com>
2025-10-08 11:35:42 -0700

    Format date, and dummy task data

--- frontend/app/(child)/(tabs)/todo.tsx
+++ frontend/app/(child)/(tabs)/todo.tsx
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
 const todo = ()=> {
 
 // Current Date State
+// useState hook to store the current date and update it... allows the app to display and maipulate the date dynamically 
   const [currentDate, setCurrentDate] = useState(new Date());
 
 // States for Categories
-const [expanded, setExpanded] = useState<{ [key: string]: boolean }>({
-  Homework: false,
-  Chores: false,
-  Completed: false,
+// expanded state object to track which categories are currently expanded or collapsed
+  const [expanded, setExpanded] = useState<{ [key: string]: boolean }>({
+    Homework: false, // All categories are collaspsed at first
+    Chores: false,
+    Completed: false,
 });
 // Helper functions for changing date
-const changeDate = (days: number) => {
-  const newDate = new Date(currentDate);
-  newDate.setDate(newDate.getDate() + days);
-  setCurrentDate(newDate);
+// Able to move back and forth between days 
+  const changeDate = (days: number) => {
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() + days);
+    setCurrentDate(newDate);
 }
-
-}
+//Format dates
+  const formattedDate = currentDate.toLocaleDateString("en-US" {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }); 
+  const categories = [
+    { id: "Homework", title: "Homework" },
+    { id: "Chores", title: "Chores" },
+    { id: "Completed", title: "Completed" },
+  ]  
+};
