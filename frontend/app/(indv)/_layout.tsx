3c38f8b7cce417f202424c27f32dec44f7eba4e2
lilistasos <anastasios.lilis@gmail.com>
2025-12-09 16:31:09 -0800

    FIX: Header missing

--- frontend/app/(indv)/_layout.tsx
+++ frontend/app/(indv)/_layout.tsx
@@ -121,6 +121,7 @@ export default function IndvLayout() {
       <Tabs
         initialRouteName="(tabs)/calendar"
         screenOptions={{
+          headerShown: true,
           tabBarActiveTintColor: headerTextColor,
           tabBarInactiveTintColor: "#555555",
           tabBarStyle: { backgroundColor: colors.primary },
